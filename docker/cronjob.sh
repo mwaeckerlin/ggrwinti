854ec42b7d698de9f6447bc6d8509332ab376067
Marc Wäckerlin <marc@jupiter>
2017-08-17 14:58:40 +0200

    fixed cron job environment

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#HERE#
+
 if test -n "$MYSQL_ENV_MYSQL_PASSWORD"; then
     update-db.sh -m | mysql -h mysql -u "${MYSQL_ENV_MYSQL_USER}" -p"${MYSQL_ENV_MYSQL_PASSWORD}" "${MYSQL_ENV_MYSQL_DATABASE}"
 else
